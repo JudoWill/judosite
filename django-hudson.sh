52d9ac2dfd0e71e74bcf26c6fa788d9c705756fb
Will Dampier <judowill@gmail.com>
2010-09-14 12:30:18 -0400

    updated

--- django-hudson.sh
+++ django-hudson.sh
@@ -2,6 +2,7 @@
 virtualenv judosite --no-site-packages
 source ./ve/bin/activate
 pip install -e git://github.com/cmheisel/nose-xcover.git#egg=nosexcover
+pip install -e http://github.com/glamkit/glamkit-eventtools.git
 pip install -q -E ./judosite -r requirements.pip
 
-python manage.py test --with-coverage --cover-package=DistAnnot --with-xunit --with-xcoverage
+python manage.py test --with-coverage --with-xunit --with-xcoverage
