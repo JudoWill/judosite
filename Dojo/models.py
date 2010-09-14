babbdea595fae965543c9714508eeb1621435351
Will Dampier <judowill@gmail.com>
2010-09-14 15:02:15 -0400

    added unicode to all of the models

--- Dojo/models.py
+++ Dojo/models.py
@@ -9,11 +9,17 @@ class PersonRecord(models.Model):
     class Meta:
         abstract = True
 
+    def _base_unicode(self, arg):
+        return '<%s:%s:%s>' % (arg, self.Person, self.DateOccured)
+
 
 class Club(models.Model):
     Name = models.CharField(max_length = 255)
     Slug = models.SlugField()
     Members = models.ManyToManyField('Person', through = 'MemberRecord')
+
+    def __unicode__(self):
+        return self.Slug
 
 class Person(models.Model):
     Name = models.CharField(max_length = 255)
@@ -27,24 +33,39 @@ class Person(models.Model):
     Picture = models.ImageField(upload_to = 'pictures', null = True,
                                 blank = True, default = None)
 
+    def __unicode__(self):
+        return self.Name
+
 class Requirement(models.Model):
     Name = models.CharField(max_length = 255)
     Slug = models.SlugField()
     URL = models.URLField(verify_exists = False)
     Valid_for = models.IntegerField()
 
+    def __unicode__(self):
+        return self.Slug
+
 class Practice(models.Model):
     Club = models.ManyToManyField(Club)
     Date = models.DateField()
+
+    def __unicode__(self):
+        return '<%s:%s>' % (self.Club, self.Date)
 
 #Record models
 class RequirementRecord(PersonRecord):
     Requirement = models.ForeignKey(Requirement)
 
 
+    def __unicode__(self):
+        return self._base_unicode(self.Requirement)
+
+
 class PracticeRecord(PersonRecord):
     Practice = models.ForeignKey(Practice)
 
+    def __unicode__(self):
+        return self._base_unicode(self.Practice)
 
     def _get_date(self):
         return self.Practice.Date
@@ -54,6 +75,9 @@ class PracticeRecord(PersonRecord):
 class MemberRecord(PersonRecord):
     Club = models.ForeignKey(Club)
     is_active = models.BooleanField(default = True)
+
+    def __unicode__(self):
+        return self._base_unicode(self.Club)
 
 class RankRecord(PersonRecord):
     rank_choices = [('White', 'White'),
@@ -69,3 +93,6 @@ class RankRecord(PersonRecord):
 
     Rank = models.CharField(max_length = 255,
                             choices = rank_choices)
+
+    def __unicode__(self):
+        return self._base_unicode(self.Rank)
