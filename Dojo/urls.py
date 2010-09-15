0f3a3eee4afb941274f7b72440bd4a654555f7fd
Will Dampier <judowill@gmail.com>
2010-09-15 12:41:17 -0400

    added a person list

--- Dojo/urls.py
+++ Dojo/urls.py
@@ -8,12 +8,7 @@ urlpatterns = patterns('',
                        url(r'^(P<club>[\w-]+)/club_detail.html', club_detail, name = 'club_detail'),
                        url(r'^(P<club>[\w-]+)/practice_list.html', practice_list, name = 'practice_list'),
                        url(r'^(P<club>[\w-]+)/(P<id>\d+)/practice_detail.html', practice_detail, name = 'practice_detail'),
-                       url(r'^(P<club>[\w-]+)/person_list.html', person_list, name = 'person_list'),
+                       url(r'^(P<club>[\w-]+)/person_list.html', person_list, name = 'person_list_by_club'),
+                       url(r'^person_list.html', person_list, name = 'person_list'),
                        url(r'^(P<club>[\w-]+)/(P<id>\d+)/person_detail.html', person_detail, name = 'person_list'),
                        )
-
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-         {'document_root': settings.STATIC_FILE_ROOT}),
-    )
