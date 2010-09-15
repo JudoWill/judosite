576005c5eaf88c4b35322eaf8e11130301cfd902
Will Dampier <judowill@gmail.com>
2010-09-15 12:22:25 -0400

    added some direct_to_template

--- Dojo/urls.py
+++ Dojo/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls.defaults import *
-from django.views.generic import list_detail
-from models import *
+from django.conf import settings
 from views import *
 
 
+urlpatterns = patterns('django.views.generic.simple',
+                       (r'^home.html',             'direct_to_template', {'template': 'index.html'}),
+                       )
 
-
-urlpatterns = patterns('',
+urlpatterns += patterns('',
                        url(r'^club_list.html', club_list, name = 'club_list'),
                        url(r'^(P<club>[\w-]+)/club_detail.html', club_detail, name = 'club_detail'),
                        url(r'^(P<club>[\w-]+)/practice_list.html', practice_list, name = 'practice_list'),
@@ -14,3 +15,9 @@ urlpatterns = patterns('',
                        url(r'^(P<club>[\w-]+)/person_list.html', person_list, name = 'person_list'),
                        url(r'^(P<club>[\w-]+)/(P<id>\d+)/person_detail.html', person_detail, name = 'person_list'),
                        )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.STATIC_FILE_ROOT}),
+    )
