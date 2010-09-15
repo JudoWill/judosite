9c475d19b352ccd409d4a543d652e8a8164e8635
Will Dampier <judowill@gmail.com>
2010-09-15 12:13:42 -0400

    added the media to the urls

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,3 +17,9 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^Dojo/', include('Dojo.urls'))
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.STATIC_FILE_ROOT}),
+    )
