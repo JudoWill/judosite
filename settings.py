a68ad808ea25ff5414216521c7b0a051a2664f48
Will Dampier <judowill@gmail.com>
2010-09-15 12:11:09 -0400

    added the template directory

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # Django settings for judosite project.
-
+import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -81,6 +81,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.abspath('templates')
 )
 
 INSTALLED_APPS = (
