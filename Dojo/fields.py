d8d8335e21b3fbd98315958b922be5916e31aac1
Will Dampier <judowill@gmail.com>
2010-09-22 12:08:02 -0400

    modified studentmulti-choice

--- Dojo/fields.py
+++ Dojo/fields.py
@@ -1,11 +1,19 @@
 from django import forms
+from django.conf import settings
 from django.db.models.query_utils import Q
 from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from models import *
 
 class StudentMultipleChoice(forms.CharField):
+
+    class Media:
+        js = (
+            settings.MEDIA_URL+'/'+'development-bundle/jquery-1.4.2.js',
+            settings.MEDIA_URL+'/'+'development-bundle/ui/jquery.ui.autocomplete.js',
+        )
+        
+
     def __init__(self, *args, **kwargs):
-        kwargs['widget'] = forms.Textarea
         super(StudentMultipleChoice, self).__init__(*args, **kwargs)
 
     def to_python(self, value):
