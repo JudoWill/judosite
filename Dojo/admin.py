12960ab5ae3bf8ec5fe052cd04fb57d0b67ece13
Will Dampier <judowill@gmail.com>
2010-09-29 11:50:33 -0400

    added a way to "join" people who are actually the same!

--- Dojo/admin.py
+++ Dojo/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from models import *
 
+
+class PersonAdmin(admin.ModelAdmin):
+    actions = ['Join_People']
+    def Join_People(self, request, queryset):
+        keep_per = queryset[0]
+        for per in queryset[1:]:
+            for prac in per.practicerecord_set.all():
+                prac.Person = keep_per
+                prac.save()
+            per.delete()
+
 admin.site.register(Club)
 
 # Debugging admin
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Requirement)
 admin.site.register(Practice)
 admin.site.register(RequirementRecord)
