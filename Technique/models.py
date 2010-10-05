799b78e84701fce3ab8dbe2857dde0c5025b23ab
Will Dampier <judowill@gmail.com>
2010-10-04 21:02:02 -0400

    misc

--- Technique/models.py
+++ Technique/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from Dojo.models import Practice
 
 # Create your models here.
+class Technique(models.Model):
+    Name = models.CharField(max_length = 255)
+    Slug = models.SlugField()
+
+    Practices = models.ManyToManyField(Practice)
