3ce260a2e76e61472a7b39928959da5679956546
Will Dampier <dampier.william@gmail.com>
2010-10-10 17:04:01 -0400

    added a template tag for getting the missing requirements for each student correctly

--- Dojo/templatetags/dojo_tags.py
+++ Dojo/templatetags/dojo_tags.py
@@ -1,6 +1,7 @@
 from Dojo.models import *
 from django import template
-
+from Dojo.utils import get_missing_reqs
+from datetime import timedelta
 register = template.Library()
 
 
@@ -8,3 +9,16 @@ register = template.Library()
 def num_classes_by_club(student, club):
     return PracticeRecord.objects.filter(Person = student, 
                                         Practice__Club = club).count()
+                                        
+@register.simple_tag
+def missing_reqs_by_club(student, club):
+    missing = []
+    for req in club.requirement_set.all():
+        last_date = date_check - timedelta(req.Valid_for)
+        qset = student.requirementrecord_set.filter(DateOccured__gte = last_date,
+                                                   Requirement = req)
+        if not qset.exists():
+            missing.append(req)
+    
+    return Requirement.objects.filter(id__in = missing)
+    
