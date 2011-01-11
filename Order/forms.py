052f44c4d2f07e7869160f7ef55cc47cbce1ed92
Will Dampier <judowill@gmail.com>
2011-01-11 15:08:47 -0500

    added a better form for creating gi-orders ... still need to do processing

--- Order/forms.py
+++ Order/forms.py
@@ -8,6 +8,18 @@ class OrderStatusForm(ModelForm):
     class Meta:
         model = OrderStatus
 
+class NOrder(forms.Form):
+    gi_type = forms.ChoiceField(choices = (('Fuji', 'Fuji'),
+                                            ('Mizuno', 'Mizuno')))
+    weave = forms.ChoiceField(choices = (('Single', 'Single'),
+                                            ('Double', 'Double'),
+                                            ('Competition', 'Competition'))
+    size = forms.IntegerField(max_value = 7, min_value = 1)
+    order_date = forms.DateField()
+    person = ModelChoiceField('student')
+
+
+
 class NewOrder(ModelForm):
     gitype = forms.ModelChoiceField(queryset = GiType.objects.all())
     person = ModelChoiceField('student')
@@ -16,4 +28,4 @@ class NewOrder(ModelForm):
     class Meta:
         model = GiOrder
         exclude = ('closed', )
-        fields = ('gitype', 'paid', 'date', 'person')
+        fields = ('paid', 'date', 'person')
