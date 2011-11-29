019019c59a470fac19526a8a39cd15361088021c
Will Dampier <dampier.william@gmail.com>
2011-11-29 11:40:48 -0500

    added a method to the manager for getting the number of practices per person over a time window

--- Dojo/managers.py
+++ Dojo/managers.py
@@ -13,3 +13,15 @@ class PersonManager(models.Manager):
 
         return qset
 
+    def time_window_qset(self, start_date, end_date, club):
+
+        qset = super(PersonManager, self).get_query_set()
+        qset = qset.filter(practicerecord__Practice__Club = club,
+                           practicerecord__Practice__Date__gte = start_date,
+                           practicerecord__Practice__Date__lte = end_date)
+        qset = qset.distinct()
+        qset = qset.annotate(num_practice=Count('practicerecord'))
+
+        return qset
+
+        
